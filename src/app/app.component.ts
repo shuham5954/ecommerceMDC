18d61f6b14ad710ea1c87b7432af08ad7a52122b
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-08-14 00:37:32 +0530

    addLogInComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogInComponent } from "./log-in/log-in.component";
+import { RouterModule , Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
