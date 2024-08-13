4de3716cb88c9d5aa948fc528ecc5d56d43c96e0
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-08-14 02:03:14 +0530

    define home login routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogInComponent } from "./log-in/log-in.component";
-import { RouterModule , Routes } from '@angular/router';
+import { RouterModule , Routes  , Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,9 @@ import { RouterModule , Routes } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router:Router){}
+swichRoute() {
+this.router.navigate(['logIn'])
+}
   title = 'ecommerceMDC';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {path:'' , redirectTo: 'home' , pathMatch:'full'},
+
+    {path:"logIn", loadComponent:()=> import('../app/log-in/log-in.component').then(res => res.LogInComponent)},
+
+    {path:"home" , loadComponent:() => import ('../app/home/home.component').then(res => res.HomeComponent)}
+
+
+];
