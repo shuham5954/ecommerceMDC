8096c581eb9ad6a996c90a30bf8468611db069c6
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-09-02 11:51:00 +0530

    changies related routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { header } from './header/header/header.routes';
 
 
 
@@ -7,6 +8,7 @@ export const routes: Routes = [
 
     {path:"" , redirectTo:'log-in' , pathMatch:'full'} , 
     {path:"log-in" , loadComponent: () => import('./log-in/log-in.component').then(c =>c.LogInComponent ) , pathMatch:'full'} , 
-    {path:"home" , loadComponent: () => import('./home/home/home.component').then(c =>c.HomeComponent ) , pathMatch:'full'} 
+    {path:"home" , loadComponent: () => import('./home/home/home.component').then(c =>c.HomeComponent ) , pathMatch:'full'} ,
+    { path: 'header', children: header }
 
 ];
--- src/app/header/header/header.routes.ts
+++ src/app/header/header/header.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from "@angular/router";
 
 
 export const header:Routes = [
-    // {path:"" , redirectTo:"cart-main" , pathMatch : 'full'},
+    {path:"" , redirectTo:"cart-main" , pathMatch : 'full'},
          {path:"" , loadComponent: () => import('./cart-main/cart-main.component').then(c =>c.CartMainComponent ) , pathMatch:'full'} , 
          {path:"chek-out-cart" , loadComponent: () => import('./check-out-cart/check-out-cart.component').then(c =>c.CheckOutCartComponent) , pathMatch:'full'} , 
          {path:"payment-cart" , loadComponent: () => import('./payment-cart/payment-cart.component').then(c =>c.PaymentCartComponent) , pathMatch:'full'} , 
