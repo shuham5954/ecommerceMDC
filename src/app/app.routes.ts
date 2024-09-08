1870f67e6daff97bbd4b2e4a8938eec657f4142a
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-09-08 18:27:02 +0530

    new routes changies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes = [
     {path:"" , redirectTo:'log-in' , pathMatch:'full'} , 
     {path:"log-in" , loadComponent: () => import('./log-in/log-in.component').then(c =>c.LogInComponent ) , pathMatch:'full'} , 
     {path:"home" , loadComponent: () => import('./home/home/home.component').then(c =>c.HomeComponent ) , pathMatch:'full'} ,
-    { path: 'header', children: header }
+    
 
 ];
