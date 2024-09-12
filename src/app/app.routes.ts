01f0db20af2458ae54bb83d9351e423c130d82df
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-09-13 00:01:36 +0530

    proper formate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 
-
-
-
 export const routes: Routes = [
-
+    
     {path:"" , redirectTo:'log-in' , pathMatch:'full'} , 
     {path:"log-in" , loadComponent: () => import('./log-in/log-in.component').then(c =>c.LogInComponent ) } , 
     {path:"home" , loadChildren: () => import('./home/home/home.routes').then(c =>c.home ) } ,
     
-
 ];
