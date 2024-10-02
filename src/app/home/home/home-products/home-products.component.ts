90cfe27260cf3adf6676d8eca42bf308bb9da7d3
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-10-03 02:20:41 +0530

    css and owl caresol add

--- src/app/home/home/home-products/home-products.component.ts
+++ src/app/home/home/home-products/home-products.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,NgModule } from '@angular/core';
 import { HeaderComponent } from "../../../header/header/header.component";
 import { NavBarComponent } from "../../../header/nav-bar/nav-bar.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
+
+// import { OwlModule } from 'ngx-owl-carousel-o';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
+import { CarouselComponent } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-products',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule,CommonModule], 
   templateUrl: './home-products.component.html',
   styleUrl: './home-products.component.css'
 })
@@ -15,7 +23,60 @@ export class HomeProductsComponent {
     console.log("hello");
     
   }
-  
-  
+
+  slidesStore = [
+    {
+      id: '1',
+      src: 'https://via.placeholder.com/300x200?text=Slide+1', // Placeholder image
+      alt: 'Description for Slide 1',
+      title: 'Slide 1 Title'
+    },
+    {
+      id: '2',
+      src: 'https://via.placeholder.com/300x200?text=Slide+2',
+      alt: 'Description for Slide 2',
+      title: 'Slide 2 Title'
+    },
+    {
+      id: '3',
+      src: 'https://via.placeholder.com/300x200?text=Slide+3',
+      alt: 'Description for Slide 3',
+      title: 'Slide 3 Title'
+    },
+    {
+      id: '4',
+      src: 'https://via.placeholder.com/300x200?text=Slide+4',
+      alt: 'Description for Slide 4',
+      title: 'Slide 4 Title'
+    }
+  ];
+
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
 
 }
+  
