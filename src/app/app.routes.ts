70e78b5cdaea304590bfa4cb8c5fe310bba7e876
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-10-19 03:15:02 +0530

    responsive logIn page HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     
-    {path:"" , redirectTo:'admin' , pathMatch:'full'} , 
+    {path:"" , redirectTo:'log-in' , pathMatch:'full'} , 
     {path:"log-in" , loadComponent: () => import('./log-in/log-in.component').then(c =>c.LogInComponent ) } , 
     {path:"home" , loadChildren: () => import('./home/home/home.routes').then(c =>c.home ) } ,
     {path:"admin" , loadChildren: () => import('./admin/admin.routes').then(c =>c.admin ) } ,
