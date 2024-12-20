34af9792214ccf995d1affc96c7a5e3b20547a09
shubham_vadhariya <vadhariyashubham@gmail.com>
2024-12-21 02:16:15 +0530

    log in functionality

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -3,103 +3,128 @@ import { Router } from '@angular/router';
 import { authService } from '../common/authService/auth.service';
 import { CommonService } from '../common/commonServices/common.service';
 import { NgClass, NgIf } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { HttpService } from '../common/commonServices/http.service';
+import { url } from 'node:inspector';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [NgIf , NgClass ,ReactiveFormsModule ],
+  imports: [NgIf, NgClass, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrl: './log-in.component.css',
 })
 export class LogInComponent implements OnInit {
-
-  public togglePageLogInSignUp:boolean = false;
+  public togglePageLogInSignUp: boolean = false;
 
   public logInForm!: FormGroup;
   public registrationForm!: FormGroup;
 
-  constructor(private router:Router , private authSer:authService , private commonS:CommonService){
-    
-  }
+  constructor(
+    private router: Router,
+    private authSer: authService,
+    private commonS: CommonService,
+    private httpc: HttpService
+  ) {}
 
-  ngOnInit(){
-  
+  ngOnInit() {
     this.createFormBuilder();
   }
 
-
-  createFormBuilder(){
-
-    // create validtion for logIn form 
+  createFormBuilder() {
+    // create validtion for logIn form
     this.logInForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
     });
 
-    // create validation for Register Page 
+    // create validation for Register Page
     this.registrationForm = new FormGroup({
-      userName: new FormControl('', [
+      user_name: new FormControl('', [
         Validators.required,
         Validators.minLength(5),
         Validators.maxLength(25),
-        Validators.pattern('^[a-zA-Z0-9]*$')
+        Validators.pattern('^[a-zA-Z0-9]*$'),
       ]),
-      mobile: new FormControl('', [Validators.required,
-        Validators.pattern(/^[0-9]{10}$/)]),
+      phone_number: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{10}$/),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required,
-        Validators.minLength(8)]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
     });
-
-
   }
 
-  onSubmit(value:any) {
+  resetForm() {
+    this.registrationForm.reset({
+      user_name: '',
+      phone_number: '',
+      email: '',
+      password: '',
+    });
+  }
 
-    if(value == 'logIn'){
+  onSubmit(value: any) {
+    console.log(value);
 
-      console.log('logIn click');
-      this.authSer.login();
-      // this.router.navigate([]);
-      this.router.navigate(['/home']);
+    if (value == 'logIn') {
+      let logInFormData = {
+        user_name: this.logInForm.value.email,
+        password: this.logInForm.value.password,
+      };
 
-      
-    // if (this.logInForm.valid) {
-    //   // admin
-    //   console.log("log in valid");
-    //   this.router.navigate(['/admin']);
-    //   // this.router.navigate(['/log-in']);
+      this.httpc
+        .post('http://127.0.0.1:8000/logIn', logInFormData)
+        .subscribe((res: any) => {
+          console.log(res);
+          
+          if (res.status_code == 200) {
+            console.log('logIn click');
+            this.authSer.login();
+            this.router.navigate(['/home']);
+          }
+        });
+    } else if (value == 'signUp') {
+      if (this.registrationForm.valid) {
+        let data = {
+          user_name: this.registrationForm.value.user_name,
+          phone_number: this.registrationForm.value.phone_number,
+          email: this.registrationForm.value.email,
+          password: this.registrationForm.value.password,
+        };
 
-    // }
-
-    }
-    
-    else if(value == 'signUp'){
-
-      if(this.registrationForm.valid){
-        
-        
+        console.log(this.registrationForm.value, 'reg form');
+        this.httpc
+          .post('http://127.0.0.1:8000/signUp', data)
+          .subscribe((res: any) => {
+            console.log(res.status_code);
+            if (res.status_code == 201) {
+              this.resetForm();
+              this.togglePageLogInSignUp = false;
+            }
+          });
       }
-     
-
     }
-
   }
 
-logIn() {
+  logIn() {
+    this.authSer.login();
 
-  this.authSer.login();
+    this.router.navigate(['/home']);
 
-  this.router.navigate(['/home']);
-
-  this.commonS.userLogIn();
-  
-  if(this.commonS.isLogIn){
     this.commonS.userLogIn();
-    console.log(this.commonS.isLogIn);  
+
+    if (this.commonS.isLogIn) {
+      this.commonS.userLogIn();
+      console.log(this.commonS.isLogIn);
+    }
   }
-}
-
-
 }
